af4bf917e9b0acb5ffea53a295bc701472c77520
alaajouri <alaa.joryi@hotmail.com>
2023-02-02 11:15:31 +0100

    lombok(getter-Setter....)

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Student A =new Student();
+        A.getId();
+        A.setId(1);
+        System.out.println(A.toString());
+
+    }
+}
